b1007523fb04f9bc2dacfbb8dfe079e75a1c4ba5
arambabudigitalbiology <ramias1099@gmail.com>
2026-09-16 10:39:15 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,29 @@
 const resourceData = {
     "class8": [
-        { chapterId: 1, chapterName: "Crop Production and Management", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 2, chapterName: "Microorganisms: Friend and Foe", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 3, chapterName: "Synthetic Fibres and Plastics", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 4, chapterName: "Materials: Metals and Non-Metals", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 5, chapterName: "Coal and Petroleum", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 6, chapterName: "Combustion and Flame", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 7, chapterName: "Conservation of Plants and Animals", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 8, chapterName: "Cell - Structure and Functions", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 9, chapterName: "Reproduction in Animals", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 10, chapterName: "Reaching the Age of Adolescence", playlistUrl: "", modules: [], quizzes: [] }
+        { chapterId: 1, chapterName: "What is Science?", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 2, chapterName: "Cell - The Basic Unit of Life", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 3, chapterName: "The World of Microorganisms Part I & II", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 4, chapterName: "Reproduction in Animals", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 5, chapterName: "The Age of Adolescence", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 6, chapterName: "Biodiversity and its Conservation", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 7, chapterName: "Different Ecosystems", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 8, chapterName: "Production of Food from Plants", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 9, chapterName: "Production of Food from Animals", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 10, chapterName: "Not for Breathing Not for Drinking", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 11, chapterName: "Why do we fall ill?", playlistUrl: "", modules: [], quizzes: [] }
     ],
     "class9": [
-        { chapterId: 1, chapterName: "Cell: Its Structure and Functions", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 1, chapterName: "Cell - Structure and Functions", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 2, chapterName: "Plant Tissues", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 3, chapterName: "Animal Tissues", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 4, chapterName: "Plasma Membrane", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 4, chapterName: "Transportation through Plasma Membrane", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 5, chapterName: "Diversity in Living Organisms", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 6, chapterName: "Sense Organs", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 7, chapterName: "Animal Behaviour", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 8, chapterName: "Challenges in Improving Agricultural Production", playlistUrl: "", modules: [], quizzes: [] }
+        { chapterId: 8, chapterName: "Challenges in Improving Agricultural Production", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 9, chapterName: "Adaptations in Different Ecosystems", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 10, chapterName: "Soil Pollution", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 11, chapterName: "Biogeochemical cycles", playlistUrl: "", modules: [], quizzes: [] }
     ],
     "class10": [
         {
@@ -32,14 +36,15 @@ const resourceData = {
                 { quizId: 2, title: "Quiz Part-2" }
             ]
         },
-        { chapterId: 2, chapterName: "Respiration", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 2, chapterName: "Respiration", playlistUrl: "https://www.youtube.com/playlist?list=PL3K...", modules: [], quizzes: [] },
         { chapterId: 3, chapterName: "Transportation", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 4, chapterName: "Excretion", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 5, chapterName: "Coordination", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 6, chapterName: "Reproduction", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 7, chapterName: "Coordination in Life Processes", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 8, chapterName: "Heredity", playlistUrl: "", modules: [], quizzes: [] },
-        { chapterId: 9, chapterName: "Our Environment", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 7, chapterName: "Coordination in life processes", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 8, chapterName: "Heredity and Evolution", playlistUrl: "", modules: [], quizzes: [] },
+        { chapterId: 9, chapterName: "Our environment", playlistUrl: "", modules: [], quizzes: [] },
         { chapterId: 10, chapterName: "Natural Resources", playlistUrl: "", modules: [], quizzes: [] }
     ]
 };
+
